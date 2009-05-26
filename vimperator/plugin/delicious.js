85cbd136da1b342ff87d3e778a481af7a487829a
Nikolai Weibull <now@bitwi.se>
2009-05-26 15:53:19 +0200

    Add completer to delicious plugin

--- vimperator/plugin/delicious.js
+++ vimperator/plugin/delicious.js
@@ -31,5 +31,37 @@ commands.addUserCommand(['deli[cious]'], 'Bookmark page at Delicious', function(
   argCount: '*',
   options: [[['-description', '-d'], commands.OPTION_STRING, null, function() [[buffer.title]]],
             [['-note', '-n'], commands.OPTION_STRING, null, null],
-            [['-private', '-p'], commands.OPTION_NOARG]]
+            [['-private', '-p'], commands.OPTION_NOARG]],
+  completer: function(context) {
+    if (context.result) {
+      context.completions = context.result;
+      return;
+    }
+
+    context.title = ['Tags', 'Type'];
+    context.incomplete = true;
+
+    var url = ['https://api.del.icio.us/v1/posts/suggest?'];
+    [['url', buffer.URL]].forEach(function(query) {
+      if (query[1])
+        url.push('&', query[0], '=', encodeURIComponent(query[1]));
+    });
+
+    var xhr = util.httpGet(url.join(""), function(xhr) {
+      context.incomplete = false;
+      var result = [];
+
+      if (xhr.status != 200) {
+        context.completions = [];
+        return;
+      }
+
+      var tags = xhr.responseXML.documentElement.getElementsByTagName('*');
+      for (var i = 0; i < tags.length; i++)
+        result.push([tags[i].textContent, tags[i].localName]);
+
+      context.completions = context.result = result;
+    });
+    context.cancel = function() xhr.abort();
+  }
 }, true);
