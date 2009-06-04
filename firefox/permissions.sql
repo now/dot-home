12a8532defb02dd53f355814dfd13c6c0d49e0a4
Nikolai Weibull <now@bitwi.se>
2009-06-04 09:37:10 +0200

    Allow shelta.se to set session cookies

--- firefox/permissions.sql
+++ firefox/permissions.sql
@@ -32,6 +32,7 @@ INSERT INTO moz_hosts (host, type, permission) VALUES('epostboxen.posten.se', 'c
 INSERT INTO moz_hosts (host, type, permission) VALUES('pragprog.com', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('reddit.com', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('residentadvisor.net', 'cookie', 1);
+INSERT INTO moz_hosts (host, type, permission) VALUES('shelta.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('skatteverket.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('sneakersnstuff.com', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('tvrage.com', 'cookie', 1);
