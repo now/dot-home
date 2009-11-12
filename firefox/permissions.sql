505a42e45a20fba32e8f3549c2c95c0d8bf556fd
Nikolai Weibull <now@bitwi.se>
2009-11-12 18:11:15 +0100

    Allow svalander.se to set session cookies

--- firefox/permissions.sql
+++ firefox/permissions.sql
@@ -58,6 +58,7 @@ INSERT INTO moz_hosts (host, type, permission) VALUES('safaribooksonline.com', '
 INSERT INTO moz_hosts (host, type, permission) VALUES('shelta.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('skatteverket.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('sneakersnstuff.com', 'cookie', 1);
+INSERT INTO moz_hosts (host, type, permission) VALUES('svalander.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('store.apple.com', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('tvrage.com', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('tumblr.com', 'cookie', 1);
