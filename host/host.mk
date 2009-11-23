ae3cca3c8d4b130d6492081cc02f3d4a7747f0c0
Nikolai Weibull <now@bitwi.se>
2009-11-23 14:37:44 +0100

    Add host-specific Zsh profile handling

--- host/host.mk
+++ host/host.mk
@@ -1,6 +1,7 @@
 host := $(shell hostname)
 
 DOTFILES = \
+	   zsh/host/profile \
 	   zsh/host/rc
 
 $(foreach file,$(DOTFILES),$(call FILE_template,$(or $(wildcard host/$(host)/$(file)),host/$(file)),$(userconfdir)/.$(file)))
