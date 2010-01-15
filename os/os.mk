c29a5d23df3e54a0315bfb2a0bac2598e7d87bec
Nikolai Weibull <now@bitwi.se>
2010-01-15 11:33:26 +0100

    Fix OS-specific building

--- os/os.mk
+++ os/os.mk
@@ -5,4 +5,4 @@ DOTFILES = \
 
 $(foreach file,$(DOTFILES),$(call FILE_template,$(or $(wildcard os/$(uname)/$(file)),os/$(file)),$(userconfdir)/.$(file)))
 
--include $(uname)/os.mk
+-include os/$(uname)/os.mk
