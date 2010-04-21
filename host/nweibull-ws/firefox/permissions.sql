b0b3012b263a5e6be81efea00e989c3acef946a2
Nikolai Weibull <now@bitwi.se>
2010-04-21 16:23:21 +0200

    Allow some internal work servers to set cookies

--- host/nweibull-ws/firefox/permissions.sql
+++ host/nweibull-ws/firefox/permissions.sql
@@ -1,4 +1,7 @@
 BEGIN TRANSACTION;
+INSERT INTO moz_hosts (host, type, permission) VALUES('172.16.0.1', 'cookie', 1);
+INSERT INTO moz_hosts (host, type, permission) VALUES('172.16.0.12', 'cookie', 1);
+INSERT INTO moz_hosts (host, type, permission) VALUES('intranet.amesto.intra', 'cookie', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('intranett.amesto.no', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('amesto.se', 'cookie', 8);
 INSERT INTO moz_hosts (host, type, permission) VALUES('sdl.com', 'cookie', 1);
