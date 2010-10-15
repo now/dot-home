a724452ee4eadf655c036ec63fa2129b97f2739e
Nikolai Weibull <now@bitwi.se>
2010-10-15 10:10:26 +0200

    Remove host-specific Firefox permissions

--- host/host.mk
+++ host/host.mk
@@ -5,8 +5,3 @@ DOTFILES = \
 	   zsh/host/rc
 
 $(foreach file,$(DOTFILES),$(call FILE_template,$(or $(wildcard host/$(host)/$(file)),host/$(file)),$(userconfdir)/.$(file)))
-
-DOTFILES = \
-	   firefox/permissions.sql
-
-$(call SQLITE_IF_EXISTS_template,$(addprefix host/$(host)/,$(DOTFILES)),$(firefoxuserconfdir),,host/$(host)/firefox/)
