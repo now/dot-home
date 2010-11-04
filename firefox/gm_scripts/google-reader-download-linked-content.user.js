8cecc8405fabb2c6c58eea557d6e6b36222e6af5
Nikolai Weibull <now@bitwi.se>
2010-11-04 11:10:23 +0100

    Update linked content handling

--- firefox/gm_scripts/google-reader-download-linked-content.user.js
+++ firefox/gm_scripts/google-reader-download-linked-content.user.js
@@ -22,7 +22,7 @@ document.addEventListener('keypress', function(event) {
   if (!post)
     return;
 
-  if (/^https?:\/\/(?:djsarepersons|beatelectric|soundsofthe70s|.*\/FeelMyBicep\/)/.test(post.href)) {
+  if (/^https?:\/\/(?:djsarepersons|beatelectric|soundsofthe70s|thenumber20|zyron|feedproxy\.google\.com\/~r\/(?:DiscoDelicious|FeelMyBicep)\/)/.test(post.href)) {
     GM_openInTab(post.href);
     return;
   }
@@ -30,15 +30,27 @@ document.addEventListener('keypress', function(event) {
   event.stopPropagation();
   event.preventDefault();
 
+  var downloaded = [];
   // /^https?:\/\/(?:(?:www\.)?divshare\.com\/direct|mp3\.juno\.co\.uk\/MP3)\//
   for (var i = 0, link; link = links.snapshotItem(i); i++) {
+    var found = false;
+    for (var j = 0; j < downloaded.length; j++)
+      if (link.href == downloaded[j]) {
+        found = true;
+        break;
+      }
+    if (found)
+      continue;
     if (/\.mp3$/.test(link.href)) {
       GM_openInTab(link.href);
+      downloaded.push(link.href);
       /*
       location.href = link.href;
       window.setTimeout(next, 500);
       */
-    } else if (/^https?:\/\/(?:www\.)?(?:divshare|mediafire|soundcloud)\.com\//.test(link.href))
+    } else if (/^https?:\/\/(?:www\.)?(?:(?:divshare|mediafire|soundcloud)\.com|(?:zshare)\.net)\//.test(link.href)) {
       GM_openInTab(link.href);
+      downloaded.push(link.href);
+    }
   }
 }, true);
