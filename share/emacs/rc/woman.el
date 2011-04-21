4ddabb1ee52e33561f7cdb229d4704e8126ede6d
Nikolai Weibull <now@bitwi.se>
2011-04-21 14:45:30 +0200

    Add missing file

---
+++ share/emacs/rc/woman.el
@@ -0,0 +1,7 @@
+(setq woman-use-own-frame nil
+      woman-bold-headings nil)
+; TODO: This is a bit overzealous, as we only want this under certain
+; conditions.  Check the evernote about a way to set this dynamically for one
+; keybinding.
+;(setq woman-use-topic-at-point t)
+
