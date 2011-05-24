08af1ec334a6bd03487ad2822e59b9f62e1bf7e1
Nikolai Weibull <now@bitwi.se>
2011-05-24 14:27:21 +0200

    Adjust formatting

--- firefox/gm_scripts/google-reader-view-original-in-background.user.js
+++ firefox/gm_scripts/google-reader-view-original-in-background.user.js
@@ -9,7 +9,11 @@ document.addEventListener('keypress', function(event) {
   if (event.which != 118)
     return;
 
-  var link = document.evaluate('(id("current-entry")//a)[1]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
+  var link = document.evaluate('(id("current-entry")//a)[1]',
+                               document,
+                               null,
+                               XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+                               null).snapshotItem(0);
   if (!link)
     return;
 
