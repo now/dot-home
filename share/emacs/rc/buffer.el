9979628069115cf4f38e930b8a003fe276b9f46d
Nikolai Weibull <now@bitwi.se>
2011-09-25 20:33:28 +0200

    Move mode-line variables to buffer rc

--- share/emacs/rc/buffer.el
+++ share/emacs/rc/buffer.el
@@ -1,1 +1,15 @@
-(setq-default indicate-buffer-boundaries '((bottom . left)))
+(setq-default indicate-buffer-boundaries '((bottom . left))
+              mode-line-remote '(:eval (if (file-remote-p default-directory) "@" ""))
+              mode-line-buffer-identification (propertized-buffer-identification "%b")
+              mode-line-frame-identification '(:eval (if (or (null window-system) (eq window-system 'pc)) "[%F] " ""))
+              mode-line-modes (butlast mode-line-modes)
+              ; TODO: 'mode-line-remote might be overkill.  Is it really
+              ; pertinent information?
+              mode-line-format (list
+                                 ""
+                                 'mode-line-client
+                                 'mode-line-remote
+                                 'mode-line-frame-identification
+                                 'mode-line-buffer-identification
+                                 '(:propertize " " 'help-echo help-echo)
+                                 'mode-line-modes))
--- share/emacs/rc/ui.el
+++ share/emacs/rc/ui.el
@@ -6,18 +6,3 @@
 (setq initial-scratch-message nil)
 
 (enable-theme 'now)
-
-(setq-default mode-line-remote '(:eval (if (file-remote-p default-directory) "@" ""))
-              mode-line-buffer-identification (propertized-buffer-identification "%b")
-              mode-line-frame-identification '(:eval (if (or (null window-system) (eq window-system 'pc)) "[%F] " ""))
-              mode-line-modes (butlast mode-line-modes)
-              ; TODO: 'mode-line-remote might be overkill.  Is it really
-              ; pertinent information?
-              mode-line-format (list
-                                 ""
-                                 'mode-line-client
-                                 'mode-line-remote
-                                 'mode-line-frame-identification
-                                 'mode-line-buffer-identification
-                                 '(:propertize " " 'help-echo help-echo)
-                                 'mode-line-modes))
