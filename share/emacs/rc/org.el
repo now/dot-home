967988557da2a4f50e8ee5adaaf8e71d2da21033
Nikolai Weibull <now@bitwi.se>
2011-09-27 16:02:29 +0200

    Fix Org mode files

--- share/emacs/rc/org.el
+++ share/emacs/rc/org.el
@@ -1,6 +1,9 @@
 (setq org-directory "~/Documents/Org"
-      org-mobile-directory "~/Dropbox/MobileOrg")
+      org-mobile-directory "~/Dropbox/MobileOrg"
+      org-refile-targets '((org-agenda-files . (:level . 1))))
 (labels ((org-file (file)
                    (concat (file-name-as-directory org-directory) file)))
-  (setq org-mobile-inbox-for-pull (org-file "flagged.org"))
-  (setq org-agenda-files (list (org-file "todo.org"))))
+  (setq org-mobile-inbox-for-pull (org-file "from-mobile.org"))
+  (setq org-agenda-files (list
+                          (org-file "music.org")
+                          (org-file "todo.org"))))
