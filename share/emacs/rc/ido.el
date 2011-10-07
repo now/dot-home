9fbdac90bd803cef47e645390868bb9112ad0144
Nikolai Weibull <now@bitwi.se>
2011-10-07 14:21:56 +0200

    Update ido settings

--- share/emacs/rc/ido.el
+++ share/emacs/rc/ido.el
@@ -2,13 +2,9 @@
 (ido-everywhere 1)
 (setq ido-enable-flex-matching t
       ido-enable-last-directory-history nil
+      ido-auto-merge-work-directories-length -1
       ido-use-filename-at-point 'guess)
-; ido-max-work-directory-list 0
-; ido-max-work-file-list 0
-; ido-show-dot-for-diret t?
 ; ido-enter-matching-directory t?
-; TODO: What does this do?
-;(setq ido-auto-merge-work-directories-length -1)
 (add-hook 'ido-setup-hook
           (lambda ()
             (define-key ido-completion-map "\C-p" 'ido-prev-match)
