7d113a036023ed444443e96bd1cf68f0def0ae9d
Nikolai Weibull <now@bitwi.se>
2011-11-01 14:57:55 +0100

    Update org mode slightly

--- share/emacs/rc/org.el
+++ share/emacs/rc/org.el
@@ -4,6 +4,11 @@
 (labels ((org-file (file)
                    (concat (file-name-as-directory org-directory) file)))
   (setq org-mobile-inbox-for-pull (org-file "from-mobile.org"))
-  (setq org-agenda-files (list
-                          (org-file "music.org")
-                          (org-file "todo.org"))))
+  (setq org-agenda-files (list org-directory)))
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (evil-define-key 'motion org-mode-map (kbd "RET") 'org-cycle)
+             (evil-define-key 'normal org-mode-map ",<" 'org-mobile-pull)
+             (evil-define-key 'normal org-mode-map ",>" 'org-mobile-push)
+             (evil-define-key 'normal org-mode-map ",t" 'org-todo)))
