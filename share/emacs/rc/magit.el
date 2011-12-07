821e84a3db5af621da4c4d7502bfed8736f5a386
Nikolai Weibull <now@bitwi.se>
2011-12-07 10:07:43 +0100

    Autoload magit

--- share/emacs/rc/magit.el
+++ share/emacs/rc/magit.el
@@ -1,2 +1,6 @@
-(require 'magit)
-(define-key magit-mode-map "q" 'close-buffer-and-window-unless-last)
+(autoload 'magit-status "magit" nil t)
+(eval-after-load 'magit
+  '(define-key magit-mode-map "q" 'close-buffer-and-window-unless-last))
+(add-hook 'magit-log-edit-mode-hook
+          (lambda ()
+            (setq fill-column 72)))
