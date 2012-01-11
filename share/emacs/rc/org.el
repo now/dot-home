af5fc843f0db7f6e7c6ebebada42383c064c2bda
Nikolai Weibull <now@bitwi.se>
2012-01-11 15:05:11 +0100

    Set org-reverse-note-order

--- share/emacs/rc/org.el
+++ share/emacs/rc/org.el
@@ -3,13 +3,14 @@
 (setq org-directory "~/Dropbox/Org"
       org-mobile-directory "~/Dropbox/MobileOrg"
       org-mobile-force-id-on-agenda-items nil
-      org-refile-targets '((org-agenda-files . (:level . 1)))
+      org-refile-targets '((org-agenda-files . (:level . 2)))
       org-refile-allow-creating-parent-nodes 'confirm
       org-outline-path-complete-in-steps nil
       org-completion-use-ido t
       org-edit-src-persistent-message nil
       org-src-window-setup 'current-window
-      org-src-fontify-natively t)
+      org-src-fontify-natively t
+      org-reverse-note-order t)
 (labels ((org-file (file)
                    (concat (file-name-as-directory org-directory) file)))
   (setq org-mobile-inbox-for-pull (org-file "from-mobile.org")
