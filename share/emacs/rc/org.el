90c613208e7fae60b48f8ca10bd3e5697d708dc7
Nikolai Weibull <now@bitwi.se>
2012-01-15 11:19:47 +0100

    Set org-log-done to 'time

--- share/emacs/rc/org.el
+++ share/emacs/rc/org.el
@@ -3,22 +3,23 @@
 (setq org-directory "~/Dropbox/Org"
       org-mobile-directory "~/Dropbox/MobileOrg"
       org-mobile-force-id-on-agenda-items nil
-      org-refile-targets '((org-agenda-files . (:level . 2)))
+      org-refile-targets '((org-agenda-files . (:level . 1)))
       org-refile-allow-creating-parent-nodes 'confirm
       org-outline-path-complete-in-steps nil
       org-completion-use-ido t
       org-edit-src-persistent-message nil
       org-src-window-setup 'current-window
       org-src-fontify-natively t
-      org-reverse-note-order t)
+      org-reverse-note-order t
+      org-log-done 'time)
 (labels ((org-file (file)
                    (concat (file-name-as-directory org-directory) file)))
   (setq org-mobile-inbox-for-pull (org-file "from-mobile.org")
         org-default-notes-file (org-file "refile.org")
         org-agenda-files (list org-directory))
   (setq org-capture-templates
-        '(("t" "Todo" entry (file "") "* TODO %?\n  %U\n  %i"))
-        '(("T" "Annotated Todo" entry (file "") "* TODO %?\n  %U\n  %i\n  %a"))))
+        '(("t" "Todo" entry (file "") "* TODO %?\n  %U\n  %i")
+          ("T" "Annotated Todo" entry (file "") "* TODO %?\n  %U\n  %i\n  %a"))))
 
 (add-hook 'org-mode-hook
           '(lambda ()
