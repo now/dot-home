0b2b933c51e9f4437e496bc9299c97b564816afc
Nikolai Weibull <now@bitwi.se>
2012-02-07 10:46:34 +0100

    Update XSL templates

--- share/emacs/rc/nxml.el
+++ share/emacs/rc/nxml.el
@@ -8,12 +8,21 @@
      (define-abbrev-table 'nxml-mode-abbrev-table ()
        "Abbrev table in use in nXML mode buffers.")
      (define-abbrev nxml-mode-abbrev-table "xsls" "" 'nxml-mode-skeleton-xsl-stylesheet)
+     (define-abbrev nxml-mode-abbrev-table "xslt" "" 'nxml-mode-skeleton-xsl-template)
      (define-skeleton nxml-mode-skeleton-xsl-stylesheet
-       "Insert a XSL Stylesheet."
+       "Insert an XSL Stylesheet."
        ""
-       > "<?xml version=\"1.0\" encoding=\"utf-8\"?>" \n
+       "<?xml version=\"1.0\" encoding=\"utf-8\"?>" \n
        > "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">" \n
        > "<xsl:output method=\"xml\" encoding=\"utf-8\"/>" \n
-       \n
        > _ \n
-       "</xsl:stylesheet>" >)))
+       "</xsl:stylesheet>" >)
+     (define-skeleton nxml-mode-skeleton-xsl-template
+       "Insert an XSL Template."
+       ""
+       > "<xsl:template match=\"" _ "\">" \n
+       > "</xsl:template>" >)))
+
+(add-hook 'nxml-mode-hook
+          '(lambda ()
+             (setq local-abbrev-table nxml-mode-abbrev-table)))
