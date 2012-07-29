ddb62bf56c07b0f6180874d8dc829f65cc892292
Nikolai Weibull <now@bitwi.se>
2012-07-29 10:40:53 +0200

    Remove Ruby flymake stuff

--- emacs.el
+++ emacs.el
@@ -608,29 +608,6 @@
        >  _ \n
        "end" > \n
        "end" >)))
-;(require 'flymake)
-;(defun flymake-ruby-init ()
-;  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-;                       'flymake-create-temp-inplace))
-;         (local-file  (file-relative-name
-;                       temp-file
-;                       (file-name-directory buffer-file-name))))
-;    (list "ruby" (list "-wc" local-file))))
-;
-;(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;
-;;; TODO: I mean, shouldn't this be local to each flymake-file-name-mask?  This
-;;; is rather stupid.
-;(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-;
-;(add-hook 'ruby-mode-hook
-;          (lambda ()
-;            ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-;            (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-;                (flymake-mode))
-;            (define-key ruby-mode-map "\C-m" 'ruby-reindent-then-newline-and-indent)
-;            ))
 
 (eval-after-load 'sh-script
   '(progn
