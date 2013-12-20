99a34057ed3e09bb16bedc1b946ac9976430f5f7
Nikolai Weibull <now@bitwi.se>
2013-12-20 16:40:26 +0100

    Update Org configuration

--- emacs.el
+++ emacs.el
@@ -249,6 +249,7 @@
 
                 "\C-d" 'suspend-frame
 
+                ",a" 'org-agenda
                 ",b" 'ido-switch-buffer
                 ",B" 'bs-show
                 ",c" 'vc-project-shell-command
@@ -373,36 +374,59 @@
      (define-key nxml-mode-map "\t" 'now-nxml-complete-or-indent-for-tab-command)
      (define-key nxml-mode-map "\C-j" 'reindent-then-newline-and-indent)))
 
+(setq org-enforce-todo-dependencies t)
+
 (eval-after-load 'org
   '(progn
-     (setq org-archive-default-command 'org-archive-to-archive-sibling
+     (defun now-refile-target-verify ()
+       (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+     (setq ;org-agenda-span 'day
+           org-archive-default-command 'org-archive-to-archive-sibling
+           org-clock-out-remove-zero-time-clocks t
            org-directory "~/Documents/Google Drive/Org"
+           org-link-frame-setup '((vm . vm-visit-folder)
+                                  (gnus . org-gnus-no-new-news)
+                                  (file . find-file)
+                                  (wl . wl))
            org-loop-over-headlines-in-active-region 'region-start-level
            org-mobile-directory "~/Sites/dav"
            org-mobile-force-id-on-agenda-items nil
-           org-refile-targets '((org-agenda-files . (:level . 1)))
            org-refile-allow-creating-parent-nodes 'confirm
+           org-refile-target-verify-function 'now-refile-target-verify
+           org-refile-targets '((org-agenda-files . (:maxlevel . 9)))
+           org-refile-use-outline-path 'file
            org-outline-path-complete-in-steps nil
            org-completion-use-ido t
            org-edit-src-persistent-message nil
            org-src-window-setup 'current-window
            org-src-fontify-natively t
+           org-treat-S-cursor-todo-selection-as-state-change nil
            org-reverse-note-order t
            org-log-done 'time
            org-log-into-drawer t)
+     ; TODO Move these to files so that MobileOrg and similar may see them
      (setq org-todo-keywords
            '((sequence "TODO(t)" "DONE(d)")
-             (sequence "WAIT(w@/!)" "NIXD(c@/!)")))
+             (sequence "WAIT(w@/!)" "DLGT(g@/!)" "NIXD(c@/!)")
+             (type "CALL")
+             (type "CHAT")))
      (setq org-todo-keyword-faces
-           '(("WAIT" . org-waiting)))
+           '(("DLGT" . org-delegated)
+             ("WAIT" . org-waiting)))
      (let ((org-file (lambda (file)
                        (concat (file-name-as-directory org-directory) file))))
        (setq org-mobile-inbox-for-pull (funcall org-file "from-mobile.org")
              org-default-notes-file (funcall org-file "refile.org")
              org-agenda-files (list org-directory)))
      (setq org-capture-templates
-           '(("t" "Todo" entry (file "") "* TODO %?\n  %U\n  %i")
-             ("T" "Annotated Todo" entry (file "") "* TODO %?\n  %U\n  %i\n  %a")))
+           '(("c" "Chat" entry (file "")
+              "* CHAT with %? :CHAT:\n%U" :clock-in t :clock-resume t)
+             ("p" "Phone Call" entry (file "")
+              "* CALL %? :CALL:\n%U" :clock-in t :clock-resume t)
+             ("t" "Todo" entry (file "")
+              "* TODO %?\n  %U\n  %i" :clock-in t :clock-resume t)
+             ("T" "Annotated Todo" entry (file "")
+              "* TODO %?\n  %U\n  %i\n  %a" :clock-in t :clock-resume t)))
      (evil-define-key 'motion org-mode-map
        (kbd "RET") 'org-cycle)
      (evil-define-key 'normal org-mode-map
--- share/emacs/site-lisp/themes/now-theme.el
+++ share/emacs/site-lisp/themes/now-theme.el
@@ -112,6 +112,7 @@
     `(org-block ((,class (:foreground "grey40"))))
     `(org-code ((,class (:foreground "grey40"))))
     `(org-date ((,class (,@blank :inherit font-lock-comment-face))))
+    `(org-delegated ((,class (:inherit org-waiting))))
     `(org-done ((,class (,@blank :foreground ,off-white :background ,green))))
     `(org-table ((,class ,blank)))
     `(org-todo ((,class (,@blank :foreground ,off-white :background ,red))))
