42d8f19ddcd403393f1ea53c9036c51e7bd0a927
Nikolai Weibull <now@bitwi.se>
2014-02-20 19:06:10 +0100

    Update Firefox permissions

--- firefox/permissions.sql
+++ firefox/permissions.sql
@@ -10,14 +10,4 @@ CREATE TABLE moz_hosts (id INTEGER PRIMARY KEY,
 INSERT INTO moz_hosts (host, type, permission) VALUES('addons.mozilla.org', 'install', 1);
 INSERT INTO moz_hosts (host, type, permission) VALUES('update.mozilla.org', 'install', 1);
 
-/* Stores */
-INSERT INTO moz_hosts (host, type, permission) VALUES('juno.co.uk', 'popup', 1);
-INSERT INTO moz_hosts (host, type, permission) VALUES('komplett.se', 'popup', 1);
-
-/* Payment */
-INSERT INTO moz_hosts (host, type, permission) VALUES('paypal.com', 'popup', 1);
-
-/* Banking */
-INSERT INTO moz_hosts (host, type, permission) VALUES('gfs.nb.se', 'popup', 1);
-
 COMMIT;
