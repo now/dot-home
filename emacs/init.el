3aed21ed8e62846652f819deb52db6459a281ffd
Nikolai Weibull <now@bitwi.se>
2014-02-26 21:30:22 +0100

    Remove some stale comments

--- emacs/init.el
+++ emacs/init.el
@@ -189,13 +189,11 @@
   '(define-key iedit-lib-keymap (kbd "M-o") 'iedit-toggle-unmatched-lines-visible))
 (global-set-key (kbd "C-x I") 'mc/mark-next-like-this)
 
-; (eval-after-load 'isearch …)
 (define-key global-map "\C-s" 'isearch-forward-regexp)
 (define-key esc-map "\C-s" 'isearch-forward)
 (define-key global-map "\C-r" 'isearch-backward-regexp)
 (define-key esc-map "\C-r" 'isearch-backward)
 
-; (eval-after-load 'nxml …)
 (add-to-list 'auto-mode-alist '("\\.xsd\\'" . nxml-mode))
 (eval-after-load "rng-loc"
   '(add-to-list 'rng-schema-locating-files (concat user-emacs-directory "etc/schema/schemas.xml")))
