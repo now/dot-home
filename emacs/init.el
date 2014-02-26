10f3dfb014aaddc69021698c89d0aff6009cb7b1
Nikolai Weibull <now@bitwi.se>
2014-02-26 21:50:35 +0100

    Remove unnecessary completion setup hook

--- emacs/init.el
+++ emacs/init.el
@@ -131,11 +131,6 @@
 
 ;;; Completion
 
-(defun now-completion-delete-prompt ()
-  (set-buffer standard-output)
-  (goto-char (point-min))
-  (delete-region (point) (search-forward "Possible completions are:\n")))
-(add-hook 'completion-setup-hook 'now-completion-delete-prompt 'append)
 (setq completion-show-help nil
       completions-format 'vertical)
 
