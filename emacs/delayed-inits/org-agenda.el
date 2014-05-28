6c875068486dd16b3625331c5e473057f542477e
Nikolai Weibull <now@bitwi.se>
2014-05-28 16:05:06 +0200

    Use children, not descendants

--- emacs/delayed-inits/org-agenda.el
+++ emacs/delayed-inits/org-agenda.el
@@ -5,9 +5,13 @@
                                      (tags      "REFILE"
                                                 ((org-agenda-overriding-header "Refilables")
                                                  (org-tags-match-list-sublevels nil)))
-                                     (tags "-NIXD"
+                                     (tags      "-NIXD"
                                                 ((org-agenda-overriding-header "Stuck Projects")
                                                  (org-agenda-skip-function 'now-org-skip-active-projects)))
+                                     (tags      "-HOLD-NIXD"
+                                                ((org-agenda-overriding-header "Projects")
+                                                 (org-agenda-skip-function 'now-org-skip-non-active-projects)
+                                                 (org-tags-match-list-sublevels 'indented)))
                                      (tags      "-REFILE/DONE|NIXD"
                                                 ((org-agenda-overriding-header "Archivables")
                                                  (org-tags-match-list-sublevels nil))))
@@ -22,23 +26,35 @@
       org-agenda-span 'day
       org-agenda-use-time-grid nil)
 
-(defun now-org-map-subtree (f)
-  "Call F for every heading underneath the current one."
+(defun now-org-each-descendant (function)
+  "Call FUNCTION for each descendant of the current heading."
   (let ((level (funcall outline-level)))
     (save-excursion
       (while (and (progn
 		    (outline-next-heading)
 		    (> (funcall outline-level) level))
 		  (not (eobp)))
-	(funcall f)))))
+	(funcall function)))))
+
+(defun now-org-each-child (function)
+  "Call FUNCTION for each child of the current heading."
+  (let* ((level (funcall outline-level))
+         (level+1 (+ level 1)))
+    (save-excursion
+      (while (and (progn
+		    (outline-next-heading)
+		    (> (funcall outline-level) level))
+		  (not (eobp)))
+        (if (= (funcall outline-level) level+1)
+            (funcall function))))))
 
 (defun now-org-project-p ()
   "Return `t' if `point' is inside a project, that is, a task
 with a subtask with a todo keyword."
   (catch 'exit
-    (now-org-map-subtree (lambda ()
-                           (if (member (org-get-todo-state) org-todo-keywords-1)
-                               (throw 'exit t))))))
+    (now-org-each-child (lambda ()
+                          (if (member (org-get-todo-state) org-todo-keywords-1)
+                              (throw 'exit t))))))
 
 (defun now-org-active-project-p (&optional known-to-be-project)
   "Return `t' if `point' is inside an active project, that is, a
@@ -46,12 +62,17 @@ project with a subtask with a todo keyword set to NEXT that
 doesn’t have a WAIT tag (inherited or otherwise)."
   (if (or known-to-be-project (now-org-project-p))
       (catch 'exit
-        (now-org-map-subtree (lambda ()
-                               (if (and (string= (org-get-todo-state) "NEXT")
-                                        (not (member "WAIT" (org-get-tags-at))))
-                                   (throw 'exit t)))))))
+        (now-org-each-child (lambda ()
+                              (if (and (string= (org-get-todo-state) "NEXT")
+                                       (not (member "WAIT" (org-get-tags-at))))
+                                  (throw 'exit t)))))))
 
 (defun now-org-skip-active-projects ()
   "Skip Org tasks that are active projects."
   (if (or (not (now-org-project-p)) (now-org-active-project-p t))
       (save-excursion (org-end-of-subtree t))))
+
+(defun now-org-skip-non-active-projects ()
+  "Skip Org tasks that aren’t projects."
+  (unless (and (now-org-project-p) (now-org-active-project-p t))
+    (save-excursion (org-end-of-subtree t))))
