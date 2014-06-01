a9324605a0ab87d4fa5335cf6dfadec9f8ef9252
Nikolai Weibull <now@disu.se>
2014-06-01 23:29:40 +0200

    Modify project definition once again

--- emacs/delayed-inits/org-agenda.el
+++ emacs/delayed-inits/org-agenda.el
@@ -5,22 +5,22 @@
                                      (tags      "REFILE"
                                                 ((org-agenda-overriding-header "Refilables")
                                                  (org-tags-match-list-sublevels nil)))
-                                     (tags      "-NIXD"
+                                     (tags-todo "-NIXD"
                                                 ((org-agenda-overriding-header "Stuck Projects")
-                                                 (org-agenda-skip-function 'now-org-skip-non-projects-and-active-projects)))
-                                     (tags      "-HOLD-NIXD"
+                                                 (org-agenda-skip-function 'now-org-skip-unless-stuck-project)))
+                                     (tags-todo "-HOLD-NIXD"
                                                 ((org-agenda-overriding-header "Projects")
-                                                 (org-agenda-skip-function 'now-org-skip-non-projects-and-stuck-projects)
+                                                 (org-agenda-skip-function 'now-org-skip-unless-active-project)
                                                  (org-tags-match-list-sublevels 'indented)))
                                      (tags-todo "-REFILE-WAIT-HOLD-NIXD/!NEXT"
-                                                ((org-agenda-overriding-header "Project Nexts")
-                                                 (org-agenda-skip-function 'now-org-skip-projects-and-standalone-tasks)
+                                                ((org-agenda-overriding-header "Next Tasks")
+                                                 (org-agenda-skip-function 'now-org-skip-unless-next-tasks)
                                                  (org-agenda-todo-ignore-scheduled t)
                                                  (org-agenda-todo-ignore-deadlines t)
                                                  (org-agenda-todo-ignore-with-date t)))
                                      (tags-todo "-REFILE-WAIT-HOLD-NIXD/!"
-                                                ((org-agenda-overriding-header "Tasks")
-                                                 (org-agenda-skip-function 'now-org-skip-projects)
+                                                ((org-agenda-overriding-header "Standalone Tasks")
+                                                 (org-agenda-skip-function 'now-org-skip-unless-tasks)
                                                  (org-agenda-todo-ignore-scheduled t)
                                                  (org-agenda-todo-ignore-deadlines t)
                                                  (org-agenda-todo-ignore-with-date t)))
@@ -57,49 +57,53 @@
       (while (org-get-next-sibling)
         (funcall function)))))
 
-(defun now-org-project-p ()
-  "Return `t' if `point' is inside a project, that is, a task
-with a subtask with a todo keyword."
+(defun now-org-find-tree (function)
+  "Find the first occurrence of a FUNCTION returning a non-nil value."
   (catch 'exit
-    (now-org-each-child (lambda ()
-                          (if (member (org-get-todo-state) org-todo-keywords-1)
-                              (throw 'exit t))))))
+    (org-map-tree (lambda () (if (funcall function) (throw 'exit t))))))
 
-(defun now-org-active-project-p (&optional known-to-be-project)
+(defun now-org-project-p ()
+  "Return `t' if `point' is inside a project, that is, a headline
+with a todo keyword and a sub-headline with a todo keyword.  This
+function assumes that `point' is at a project task."
+  (catch 'exit
+    (now-org-each-descendant (lambda ()
+                               (if (org-get-todo-state)
+                                   (throw 'exit t))))))
+
+(defun now-org-active-project-p ()
   "Return `t' if `point' is inside an active project, that is, a
-project with a subtask with a todo keyword set to NEXT that
-doesn’t have a WAIT tag (inherited or otherwise)."
-  (if (or known-to-be-project (now-org-project-p))
-      (catch 'exit
-        (now-org-each-child (lambda ()
-                              (if (and (string= (org-get-todo-state) "NEXT")
-                                       (not (member "WAIT" (org-get-tags-at))))
-                                  (throw 'exit t)))))))
+project with a sub-task with a todo keyword set to NEXT that
+doesn’t have a WAIT tag (inherited or direct).  This function
+assumes that `point' is at a project task, that is, a headline
+with a todo keyword."
+  (catch 'exit
+    (now-org-each-descendant (lambda ()
+                               (if (and (string= (org-get-todo-state) "NEXT")
+                                        (not (member "WAIT" (org-get-tags-at))))
+                                   (throw 'exit t))))))
 
-(defun now-org-skip-non-projects-and-active-projects ()
+(defun now-org-skip-unless-stuck-project ()
   "Skip Org tasks that aren’t projects or that are projects that are active."
-  (if (or (not (now-org-project-p)) (now-org-active-project-p t))
+  (if (or (now-org-active-project-p) (not (now-org-project-p)))
       (save-excursion (or (outline-next-heading) (point-max)))))
 
-(defun now-org-skip-non-projects-and-stuck-projects ()
+(defun now-org-skip-unless-active-project ()
   "Skip Org tasks that aren’t projects or that are projects that aren’t active."
-  (unless (and (now-org-project-p) (now-org-active-project-p t))
+  (unless (now-org-active-project-p)
     (save-excursion (or (outline-next-heading) (point-max)))))
 
 (defun now-org-project-task-p ()
   "Return `t' if `point' is inside a project."
-  (catch 'exit
-    (while (org-up-heading-safe)
-      (if (now-org-project-p)
-          (throw 'exit t)))))
+  (save-excursion
+    (and (org-up-heading-safe) (org-get-todo-state))))
 
-(defun now-org-skip-projects-and-standalone-tasks ()
+(defun now-org-skip-unless-next-tasks ()
   "Skip tasks that are projects or that are standalone tasks."
-  (message "testing %s" (nth 4 (org-heading-components)))
   (if (or (now-org-project-p) (not (now-org-project-task-p)))
       (save-excursion (or (outline-next-heading) (point-max)))))
 
-(defun now-org-skip-projects ()
+(defun now-org-skip-unless-tasks ()
   "Skip tasks that are projects."
   (if (or (now-org-project-p) (now-org-project-task-p))
       (save-excursion (org-end-of-subtree t))))
