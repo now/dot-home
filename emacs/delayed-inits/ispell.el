ce1b245afd54597dc2b37c2852a68e1c120d7759
Nikolai Weibull <now@disu.se>
2014-06-07 21:53:53 +0200

    Make ispell adjustments for handling ‘’’ correctly

--- emacs/delayed-inits/ispell.el
+++ emacs/delayed-inits/ispell.el
@@ -1,1 +1,6 @@
 (setq-default ispell-check-comments 'exclusive)
+(setq ispell-local-dictionary-alist
+      '((nil "[[:alpha:]]" "[^[:alpha:]]" "['’]" nil ("-B") nil utf-8)
+        ("american" "[[:alpha:]]" "[^[:alpha:]]" "['’]" nil ("-B") nil utf-8)
+        ("british" "[[:alpha:]]" "[^[:alpha:]]" "['’]" nil ("-B") nil utf-8)
+        ("svenska" "[[:alpha:]]" "[^[:alpha:]]" "['’]" nil ("-C") nil utf-8)))
