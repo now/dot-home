953fd20fb0ab281cbf827287ce3163b1141ba555
Nikolai Weibull <now@bitwi.se>
2014-07-08 10:42:46 +0200

    Add @SHELL@ and @ZSHELL@ to sh-alias-alist

--- emacs/delayed-inits/sh-script.el
+++ emacs/delayed-inits/sh-script.el
@@ -1,4 +1,5 @@
 (setq-default sh-indentation 2
               sh-basic-offset 2)
 (define-key sh-mode-map "\C-j" 'reindent-then-newline-and-indent)
-
+(add-to-list 'sh-alias-alist '(@SHELL@ . sh))
+(add-to-list 'sh-alias-alist '(@ZSHELL@ . zsh))
