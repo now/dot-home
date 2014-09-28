d60866a3dfc17b77285e913dc6824dae5568f54a
Nikolai Weibull <now@disu.se>
2014-09-28 19:42:50 +0200

    Use vertical ido

--- emacs/delayed-inits/ido.el
+++ emacs/delayed-inits/ido.el
@@ -1,5 +1,10 @@
 (ido-everywhere 1)
-(setq ido-enable-flex-matching t
+(setq ido-auto-merge-work-directories-length -1
+      ido-decorations (append '("\n" "" "\n" "\n…") (nthcdr 4 ido-decorations))
+      ido-enable-flex-matching t
       ido-enable-last-directory-history nil
-      ido-auto-merge-work-directories-length -1
+      ido-max-prospects 70
+      ido-max-window-height 0.99
       ido-use-filename-at-point nil)
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
