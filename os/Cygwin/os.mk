8bb5ce3f6c1a683d6a22c01cb49f7bff8a2c2ffc
Nikolai Weibull <now@disu.se>
2014-10-23 10:27:26 +0200

    Use new template for some additional Cygwin directories

--- os/Cygwin/os.mk
+++ os/Cygwin/os.mk
@@ -1,4 +1,4 @@
-firefoxprofilesdir=$(appdatadir)/Mozilla/Firefox
+firefoxprofilesdir = $(appdatadir)/Mozilla/Firefox
 
 firefoxprofiles_DATA = \
 	os/Cygwin/firefox/profiles.ini
@@ -12,57 +12,113 @@ userconf_DATA += \
 	os/Cygwin/hotkeys.ahk \
 	os/Cygwin/minttyrc
 
-appdata_DATA = \
+appdataghislerdir = $(appdata)/GHISLER
+appdataghisler_DATA = \
 	os/Cygwin/GHISLER/lsplugin.ini \
 	os/Cygwin/GHISLER/no.bar \
-	os/Cygwin/GHISLER/packers/rar/default.sfx \
-	os/Cygwin/GHISLER/packers/rar/wincon.sfx \
-	os/Cygwin/GHISLER/packers/rar/zip.sfx \
-	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.ini \
-	os/Cygwin/GHISLER/plugins/wfx/registry/English.lng \
-	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.ini \
 	os/Cygwin/GHISLER/tcignore.txt \
 	os/Cygwin/GHISLER/totalcmd.ini \
 	os/Cygwin/GHISLER/usercmd.ini \
 	os/Cygwin/GHISLER/work.ini
 
-appdata_PROGRAMS = \
+appdataghislerlanguagesdir = $(appdataghislerdir)/languages
+appdataghislerlanguages_DATA = \
 	os/Cygwin/GHISLER/languages/wcmd_now.lng \
-	os/Cygwin/GHISLER/languages/wcmd_now.mnu \
+	os/Cygwin/GHISLER/languages/wcmd_now.mnu
+
+appdataghislerpackersrardir = $(appdataghislerdir)/packers/rar
+appdataghislerpackersrar_DATA = \
+	os/Cygwin/GHISLER/packers/rar/default.sfx \
+	os/Cygwin/GHISLER/packers/rar/wincon.sfx \
+	os/Cygwin/GHISLER/packers/rar/zip.sfx
+appdataghislerpackersrar_SCRIPTS = \
 	os/Cygwin/GHISLER/packers/rar/rar.exe \
-	os/Cygwin/GHISLER/packers/rar/unrar.exe \
-	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.wcx \
-	os/Cygwin/GHISLER/plugins/wcx/iso/iso.wcx \
-	os/Cygwin/GHISLER/plugins/wcx/targzbz2/targzbz2.wcx \
-	os/Cygwin/GHISLER/plugins/wdx/encoding/encoding.wdx \
-	os/Cygwin/GHISLER/plugins/wdx/unicodetest/unicodetest.wdx \
-	os/Cygwin/GHISLER/plugins/wfx/environment/environment.wfx \
-	os/Cygwin/GHISLER/plugins/wfx/registry/registry.wfx \
-	os/Cygwin/GHISLER/plugins/wlx/gswlx/gswlx.wlx \
+	os/Cygwin/GHISLER/packers/rar/unrar.exe
+
+appdataghislerpluginsdir = $(appdataghislerdir)/plugins
+appdataghislerpluginswcxdir = $(appdataghislerpluginsdir)/wcx
+appdataghislerpluginswdxdir = $(appdataghislerpluginsdir)/wdx
+appdataghislerpluginswfxdir = $(appdataghislerpluginsdir)/wfx
+appdataghislerpluginswlxdir = $(appdataghislerpluginsdir)/wlx
+
+appdataghislerpluginswcx7zipdir = $(appdataghislerpluginswcxdir)/7zip
+appdataghislerpluginswcx7zip_DATA = \
+	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.ini
+appdataghislerpluginswcx7zip_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.wcx
+
+appdataghislerpluginswcxisodir = $(appdataghislerpluginswcxdir)/iso
+appdataghislerpluginswcxiso_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wcx/iso/iso.wcx
+
+appdataghislerpluginswcxtargzbz2dir = $(appdataghislerpluginswcxdir)/targzbz2
+appdataghislerpluginswcxtargzbz2_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wcx/targzbz2/targzbz2.wcx
+
+appdataghislerpluginswdxencodingdir = $(appdataghislerpluginswdxdir)/encoding
+appdataghislerpluginswdxencoding_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wdx/encoding/encoding.wdx
+
+appdataghislerpluginswdxunicodetestdir = $(appdataghislerpluginswdxdir)/unicodetest
+appdataghislerpluginswdxunicodetest_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wdx/unicodetest/unicodetest.wdx
+
+appdataghislerpluginswfxenvironmentdir = $(appdataghislerpluginswfxdir)/environment
+appdataghislerpluginswfxenvironment_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wfx/environment/environment.wfx
+
+appdataghislerpluginswfxregistrydir = $(appdataghislerpluginswfxdir)/registry
+appdataghislerpluginswfxregistry_DATA = \
+	os/Cygwin/GHISLER/plugins/wfx/registry/English.lng
+appdataghislerpluginswfxregistry_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wfx/registry/registry.wfx
+
+appdataghislerpluginswlxgswlxdir = $(appdataghislerpluginswlxdir)/gswlx
+appdataghislerpluginswlxgswlx_SCRIPTS = \
+	os/Cygwin/GHISLER/plugins/wlx/gswlx/gswlx.wlx
+
+appdataghislerpluginswlximaginedir = $(appdataghislerpluginswlxdir)/imagine
+appdataghislerpluginswlximagine_DATA = \
+	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.ini
+appdataghislerpluginswlximagine_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.dll \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.wcx \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.wlx \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/plugin/j2k.dll \
-	os/Cygwin/GHISLER/plugins/wlx/imagine/plugin/jbig.dll \
+	os/Cygwin/GHISLER/plugins/wlx/imagine/plugin/jbig.dll
+
+appdataghislertoolsworkdir = $(appdataghislerdir)/tools/work
+appdataghislertoolswork_SCRIPTS = \
 	os/Cygwin/GHISLER/tools/work/open-in-tageditor-non-retardedly.vbs
 
-$(eval $(call GROUP_template,$(appdata_DATA),$(appdatadir),,os/Cygwin/))
-$(eval $(call GROUP_template,$(appdata_PROGRAMS),$(appdatadir),,os/Cygwin/,755))
+$(call DIR,appdataghisler)
+$(call DIR,appdataghislerlanguages)
+$(call DIR,appdataghislerpackersrar)
+$(call DIR,appdataghislerpluginswcx7zip)
+$(call DIR,appdataghislerpluginswcxiso)
+$(call DIR,appdataghislerpluginswcxtargzbz2)
+$(call DIR,appdataghislerpluginswdxencoding)
+$(call DIR,appdataghislerpluginswdxunicodetest)
+$(call DIR,appdataghislerpluginswfxenvironment)
+$(call DIR,appdataghislerpluginswfxregistry)
+$(call DIR,appdataghislerpluginswlxgswlx)
+$(call DIR,appdataghislerpluginswlximagine)
+$(call DIR,appdataghislertoolswork)
 
-appdata_DATA = \
+appdatadialogdeathdir = $(appdatadir)/Dialog\ Death
+appdatadialogdeath_DATA = \
 	os/Cygwin/dialog-death.ini
 
-$(eval $(call GROUP_template,$(appdata_DATA),$(appdatadir)/Dialog\ Death,,os/Cygwin/))
+$(call DIR,appdatadialogdeath)
 
-startup_PROGRAMS = \
+startupdir = $(call shell_quote,$(shell cygpath -P))/Startup
+startup_SCRIPTS = \
 	os/Cygwin/start-up/clipx.lnk \
 	os/Cygwin/start-up/cygwin.lnk \
 	os/Cygwin/start-up/hotkeys.lnk \
 	os/Cygwin/start-up/totalcmd.lnk
 
-startupdir = $(call shell_quote,$(shell cygpath -P))/Startup
-
-$(eval $(call GROUP_template,$(startup_PROGRAMS),$(startupdir),,os/Cygwin/start-up/,755))
+$(call DIR,startup)
 
 share_DATA = \
 	os/Cygwin/share/icons/bak.ico \
