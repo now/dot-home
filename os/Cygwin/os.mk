ae355c9762e5ea4665244f98b80a343c65e5ec94
Nikolai Weibull <now@disu.se>
2014-10-26 23:13:25 +0100

    Move directories to top

--- os/Cygwin/os.mk
+++ os/Cygwin/os.mk
@@ -6,6 +6,26 @@ userconf_DATA += \
 	os/Cygwin/minttyrc
 
 appdataghislerdir = $(appdata)/GHISLER
+appdataghislerlanguagesdir = $(appdataghislerdir)/languages
+appdataghislerpackersrardir = $(appdataghislerdir)/packers/rar
+appdataghislerpluginsdir = $(appdataghislerdir)/plugins
+appdataghislerpluginswcxdir = $(appdataghislerpluginsdir)/wcx
+appdataghislerpluginswcx7zipdir = $(appdataghislerpluginswcxdir)/7zip
+appdataghislerpluginswcxisodir = $(appdataghislerpluginswcxdir)/iso
+appdataghislerpluginswcxtargzbz2dir = $(appdataghislerpluginswcxdir)/targzbz2
+appdataghislerpluginswdxdir = $(appdataghislerpluginsdir)/wdx
+appdataghislerpluginswdxencodingdir = $(appdataghislerpluginswdxdir)/encoding
+appdataghislerpluginswdxunicodetestdir = $(appdataghislerpluginswdxdir)/unicodetest
+appdataghislerpluginswfxdir = $(appdataghislerpluginsdir)/wfx
+appdataghislerpluginswfxenvironmentdir = $(appdataghislerpluginswfxdir)/environment
+appdataghislerpluginswfxregistrydir = $(appdataghislerpluginswfxdir)/registry
+appdataghislerpluginswlxdir = $(appdataghislerpluginsdir)/wlx
+appdataghislerpluginswlxgswlxdir = $(appdataghislerpluginswlxdir)/gswlx
+appdataghislerpluginswlximaginedir = $(appdataghislerpluginswlxdir)/imagine
+appdataghislertoolsworkdir = $(appdataghislerdir)/tools/work
+appdatadialogdeathdir = $(appdatadir)/Dialog\ Death
+startupdir = $(call shell_quote,$(shell cygpath -P))/Startup
+
 appdataghisler_DATA = \
 	os/Cygwin/GHISLER/lsplugin.ini \
 	os/Cygwin/GHISLER/no.bar \
@@ -14,65 +34,52 @@ appdataghisler_DATA = \
 	os/Cygwin/GHISLER/usercmd.ini \
 	os/Cygwin/GHISLER/work.ini
 
-appdataghislerlanguagesdir = $(appdataghislerdir)/languages
 appdataghislerlanguages_DATA = \
 	os/Cygwin/GHISLER/languages/wcmd_now.lng \
 	os/Cygwin/GHISLER/languages/wcmd_now.mnu
 
-appdataghislerpackersrardir = $(appdataghislerdir)/packers/rar
 appdataghislerpackersrar_DATA = \
 	os/Cygwin/GHISLER/packers/rar/default.sfx \
 	os/Cygwin/GHISLER/packers/rar/wincon.sfx \
 	os/Cygwin/GHISLER/packers/rar/zip.sfx
+
 appdataghislerpackersrar_SCRIPTS = \
 	os/Cygwin/GHISLER/packers/rar/rar.exe \
 	os/Cygwin/GHISLER/packers/rar/unrar.exe
 
-appdataghislerpluginsdir = $(appdataghislerdir)/plugins
-appdataghislerpluginswcxdir = $(appdataghislerpluginsdir)/wcx
-appdataghislerpluginswdxdir = $(appdataghislerpluginsdir)/wdx
-appdataghislerpluginswfxdir = $(appdataghislerpluginsdir)/wfx
-appdataghislerpluginswlxdir = $(appdataghislerpluginsdir)/wlx
-
-appdataghislerpluginswcx7zipdir = $(appdataghislerpluginswcxdir)/7zip
 appdataghislerpluginswcx7zip_DATA = \
 	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.ini
+
 appdataghislerpluginswcx7zip_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wcx/7zip/7zip.wcx
 
-appdataghislerpluginswcxisodir = $(appdataghislerpluginswcxdir)/iso
 appdataghislerpluginswcxiso_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wcx/iso/iso.wcx
 
-appdataghislerpluginswcxtargzbz2dir = $(appdataghislerpluginswcxdir)/targzbz2
 appdataghislerpluginswcxtargzbz2_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wcx/targzbz2/targzbz2.wcx
 
-appdataghislerpluginswdxencodingdir = $(appdataghislerpluginswdxdir)/encoding
 appdataghislerpluginswdxencoding_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wdx/encoding/encoding.wdx
 
-appdataghislerpluginswdxunicodetestdir = $(appdataghislerpluginswdxdir)/unicodetest
 appdataghislerpluginswdxunicodetest_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wdx/unicodetest/unicodetest.wdx
 
-appdataghislerpluginswfxenvironmentdir = $(appdataghislerpluginswfxdir)/environment
 appdataghislerpluginswfxenvironment_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wfx/environment/environment.wfx
 
-appdataghislerpluginswfxregistrydir = $(appdataghislerpluginswfxdir)/registry
 appdataghislerpluginswfxregistry_DATA = \
 	os/Cygwin/GHISLER/plugins/wfx/registry/English.lng
+
 appdataghislerpluginswfxregistry_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wfx/registry/registry.wfx
 
-appdataghislerpluginswlxgswlxdir = $(appdataghislerpluginswlxdir)/gswlx
 appdataghislerpluginswlxgswlx_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wlx/gswlx/gswlx.wlx
 
-appdataghislerpluginswlximaginedir = $(appdataghislerpluginswlxdir)/imagine
 appdataghislerpluginswlximagine_DATA = \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.ini
+
 appdataghislerpluginswlximagine_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.dll \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/imagine.wcx \
@@ -80,15 +87,12 @@ appdataghislerpluginswlximagine_SCRIPTS = \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/plugin/j2k.dll \
 	os/Cygwin/GHISLER/plugins/wlx/imagine/plugin/jbig.dll
 
-appdataghislertoolsworkdir = $(appdataghislerdir)/tools/work
 appdataghislertoolswork_SCRIPTS = \
 	os/Cygwin/GHISLER/tools/work/open-in-tageditor-non-retardedly.vbs
 
-appdatadialogdeathdir = $(appdatadir)/Dialog\ Death
 appdatadialogdeath_DATA = \
 	os/Cygwin/dialog-death.ini
 
-startupdir = $(call shell_quote,$(shell cygpath -P))/Startup
 startup_SCRIPTS = \
 	os/Cygwin/start-up/clipx.lnk \
 	os/Cygwin/start-up/cygwin.lnk \
