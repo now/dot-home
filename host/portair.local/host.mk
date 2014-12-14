f0d043b09cbad7e7bc516b7a8f15cb463d7ab879
Nikolai Weibull <now@disu.se>
2014-12-15 00:01:32 +0100

    Change Apache configuration

--- host/portair.local/host.mk
+++ host/portair.local/host.mk
@@ -1,14 +1,11 @@
 sysconfapache2dir = $(sysconfdir)/apache2
-sysconfapache2extradir = $(sysconfapache2dir)/extra
 sysconfapache2usersdir = $(sysconfapache2dir)/users
-
-sysconfapache2extra_DATA = \
-	host/portair.local/etc/apache2/extra/httpd-vhosts.conf
 
 sysconfapache2users_DATA = \
 	host/portair.local/etc/apache2/users/now.conf
 
 sysconf_PATCHES = \
+	host/portair.local/etc/apache2/extra/httpd-userdir.conf.patch \
 	host/portair.local/etc/apache2/httpd.conf.patch \
 	host/portair.local/etc/hosts.patch
 
