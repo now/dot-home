6bc48a73e92b970bf0d0f810ff590c5e9a10319e
Nikolai Weibull <now@disu.se>
2015-01-04 18:54:43 +0100

    Move Zsh OS stuff to Zsh directory

--- os/os.mk
+++
@@ -1,9 +0,0 @@
-xdgconfighomezshosdir = $(xdgconfighomezshdir)/os
-xdgconfighomezshos_DATA = \
-	os/zsh/env \
-	os/zsh/login \
-	os/zsh/rc
-
--include $(srcdir)/os/$(uname)/os.mk
-
-$(call DIR,xdgconfighomezshos)
