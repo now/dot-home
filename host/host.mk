8ebc82f4312591d2e0714ce3a4788d2e4e770ae7
Nikolai Weibull <now@disu.se>
2015-01-04 18:58:12 +0100

    Move Zsh host stuff to Zsh directory

--- host/host.mk
+++
@@ -1,9 +0,0 @@
-host := $(shell hostname)
-
-xdgconfighomezshhostdir = $(xdgconfighomezshdir)/host
-xdgconfighomezshhost_DATA = \
-	host/zsh/profile
-
--include $(srcdir)/host/$(host)/host.mk
-
-$(call DIR,xdgconfighomezshhost)
