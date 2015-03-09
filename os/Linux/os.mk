36b8ed7fd147c3a7a4b4a0a83ea51868a12f4ff1
Nikolai Weibull <now@disu.se>
2015-03-09 22:10:44 +0100

    Remove xdigraph

--- os/Linux/os.mk
+++ os/Linux/os.mk
@@ -9,18 +9,4 @@ userconfxmonaddir = $(userconf)/xmonad
 userconfxmonad_DATA = \
 	os/Linux/xmonad/xmonad.hs
 
-HAVE_X11 := $(realpath /usr/X11)
-
-ifneq ($(HAVE_X11),)
-oslinuxbin_programs = \
-	os/Linux/bin/xdigraph
-
-$(oslinuxbin_programs): os/Linux/bin/.dirstamp
-
-LDLIBS = -lX11
-
-bin_PROGRAMS += \
-	$(oslinuxbin_programs)
-endif
-
 $(call DIR,userconfxmonad)
