ddd23d321637a02d919d8dd8bf710670499461d3
Nikolai Weibull <now@disu.se>
2015-05-18 11:15:06 +0200

    Add term-mode to evil-emacs-state-modes

--- emacs.d/delayed-inits/evil.el
+++ emacs.d/delayed-inits/evil.el
@@ -15,6 +15,7 @@
       evil-move-cursor-back nil)
 
 (add-to-list 'evil-emacs-state-modes 'git-rebase-mode)
+(add-to-list 'evil-emacs-state-modes 'term-mode)
 
 (cl-labels ((define-keys (map key def &rest bindings)
               (define-key map key def)
