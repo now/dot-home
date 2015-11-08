81362a5af3f79b934fa4898a75c35839ce6a0f76
Nikolai Weibull <now@disu.se>
2015-11-08 21:16:35 +0100

    Don’t nag about pushing to upstream

--- emacs.d/delayed-inits/magit.el
+++ emacs.d/delayed-inits/magit.el
@@ -1,3 +1,4 @@
-(setq magit-repository-directories '("~/Projects")
+(setq magit-push-always-verify nil
+      magit-repository-directories '("~/Projects")
       magit-repository-directories-depth 1
       magit-revert-buffers 'silent)
