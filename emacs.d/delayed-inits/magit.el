5f348463e579948b0f4cf23887fe42240eb7b378
Nikolai Weibull <now@disu.se>
2015-12-07 21:38:35 +0100

    Remove unused magit-push-always-verify

--- emacs.d/delayed-inits/magit.el
+++ emacs.d/delayed-inits/magit.el
@@ -1,5 +1,4 @@
-(setq magit-push-always-verify nil
-      magit-repository-directories '("~/Projects")
+(setq magit-repository-directories '("~/Projects")
       magit-repository-directories-depth 1
       magit-revert-buffers 'silent)
 (add-hook 'magit-mode-hook 'now-do-not-show-trailing-whitespace)
