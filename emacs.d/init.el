acd384fb86138ae4f3118f5cf551ae6853389d4d
Nikolai Weibull <now@disu.se>
2015-12-20 17:58:19 +0100

    Move rename-shows to site-lisp/now-simple.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,30 +25,3 @@
 (add-to-list 'auto-mode-alist (cons (purecopy "\\.xsd\\'") 'xml-mode))
 (add-to-list 'auto-mode-alist (cons (purecopy "\\.at\\'") 'm4-mode))
 (add-to-list 'auto-mode-alist (cons (purecopy "\\.jsx\(inc\)?\\'") 'js-mode))
-
-(defun rename-shows ()
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (goto-char 1)
-      (re-search-forward "^[^/]")
-      (let ((n (- (count-lines 1 (point)) 1))
-            (m (count-lines (point) (point-max))))
-        (if (not (= n m))
-            (error "Number of files doesn’t match number of names: %d≠%d" n m))
-        (dotimes (i n)
-          (goto-char 1)
-          (beginning-of-line (+ n 1))
-          (let ((line (downcase (replace-regexp-in-string ".*\"\\([^\"]+\\)\".*" "\\1"
-                                                          (delete-and-extract-region (point) (line-end-position))
-                                                          t))))
-            (unless (eobp)
-              (delete-char 1))
-            (goto-char 1)
-            (beginning-of-line (+ i 1))
-            (re-search-forward "^/\\(.*?E\\([0-9]+\\).*?\\)\\.[^./]*/")
-            (replace-match (concat "\\2-"
-                                     (save-match-data
-                                       (dolist (r '(("'" . "’") (" " . "_") ("\\.\\.\\." . "…") ("?" . "") ("&" . "and") ("!" . "") ("$" . "s") ("\\\\" "\\\\")) line)
-                                         (setq line (replace-regexp-in-string (car r) (cdr r) line t t)))))
-                             t nil nil 1)))))))
