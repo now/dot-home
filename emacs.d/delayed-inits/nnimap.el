d878ac75d0c24962dd779cf1e8e27584f7500632
Nikolai Weibull <now@disu.se>
2016-01-04 19:20:54 +0100

    Factor out nnimap stuff to fix bugs

---
+++ emacs.d/delayed-inits/nnimap.el
@@ -0,0 +1,25 @@
+(require 'gnus-sum)
+(defun now-gnus-summary-mute-articles (n)
+  (interactive "P")
+  (let ((articles (list))
+        thread-starters)
+    (dolist (article (gnus-summary-work-articles n))
+      (unless (member article articles)
+        (let ((parent (gnus-summary-article-parent article)))
+          (while parent
+            (setq article parent)
+            (setq parent (gnus-summary-article-parent parent))))
+        (push article thread-starters)
+        (setq articles (append articles (gnus-summary-articles-in-thread article)))))
+    (sort articles '<)
+    (when (with-current-buffer (nnimap-buffer)
+            (and (nnimap-command "UID STORE %s +X-GM-LABELS (\\Muted)"
+                                 (nnimap-article-ranges articles))
+                 (nnimap-command "UID STORE %s -X-GM-LABELS (\\Inbox)"
+                                 (nnimap-article-ranges articles))))
+      (apply 'gnus-summary-remove-process-mark articles)
+      (dolist (article thread-starters)
+        (gnus-summary-goto-subject article)
+        (gnus-summary-kill-thread)))))
+
+(define-key gnus-summary-mode-map "m" 'now-gnus-summary-mute-articles)
