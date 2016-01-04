85a4b7992d283092f566f9cda86084c05247fd0b
Nikolai Weibull <now@disu.se>
2016-01-04 19:40:34 +0100

    Make now-gnus-summary-mute-articles more robust

--- emacs.d/delayed-inits/nnimap.el
+++ emacs.d/delayed-inits/nnimap.el
@@ -1,24 +1,26 @@
 (require 'gnus-sum)
 (defun now-gnus-summary-mute-articles (n)
   (interactive "P")
-  (let ((articles (list))
-        thread-starters)
+  (let (articles root-articles)
     (dolist (article (gnus-summary-work-articles n))
       (unless (member article articles)
         (let ((parent (gnus-summary-article-parent article)))
           (while parent
             (setq article parent)
             (setq parent (gnus-summary-article-parent parent))))
-        (push article thread-starters)
-        (setq articles (append articles (gnus-summary-articles-in-thread article)))))
+        (push article root-articles)
+        (if articles
+            (nconc articles (gnus-summary-articles-in-thread article))
+          (setq articles (gnus-summary-articles-in-thread article)))))
     (sort articles '<)
-    (when (with-current-buffer (nnimap-buffer)
-            (and (nnimap-command "UID STORE %s +X-GM-LABELS (\\Muted)"
-                                 (nnimap-article-ranges articles))
-                 (nnimap-command "UID STORE %s -X-GM-LABELS (\\Inbox)"
-                                 (nnimap-article-ranges articles))))
+    (when (and articles
+               (with-current-buffer (nnimap-buffer)
+                 (and (nnimap-command "UID STORE %s +X-GM-LABELS (\\Muted)"
+                                      (nnimap-article-ranges articles))
+                      (nnimap-command "UID STORE %s -X-GM-LABELS (\\Inbox)"
+                                      (nnimap-article-ranges articles)))))
       (apply 'gnus-summary-remove-process-mark articles)
-      (dolist (article thread-starters)
+      (dolist (article (nreverse root-articles))
         (gnus-summary-goto-subject article)
         (gnus-summary-kill-thread)))))
 
