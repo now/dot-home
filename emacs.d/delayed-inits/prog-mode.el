86ed1802f45ffb3e54c7292a9b82312912d22024
Nikolai Weibull <now@disu.se>
2017-03-05 15:53:46 +0100

    Add now-turn-on-whitespace-mode to prog-mode.el

--- emacs.d/delayed-inits/prog-mode.el
+++ emacs.d/delayed-inits/prog-mode.el
@@ -1,3 +1,4 @@
 (add-hook 'prog-mode-hook 'now-comment-auto-fill-only-comments)
-(add-hook 'prog-mode-hook 'now-show-trailing-whitespace)
+;(add-hook 'prog-mode-hook 'now-show-trailing-whitespace)
 (add-hook 'prog-mode-hook 'turn-on-auto-fill)
+(add-hook 'prog-mode-hook 'now-turn-on-whitespace-mode)
