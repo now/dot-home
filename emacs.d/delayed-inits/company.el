ada8babbbedb91ad1f794120462b3cc9a366e6b7
Nikolai Weibull <now@disu.se>
2017-08-15 10:31:15 +0200

    Update company-backends

--- emacs.d/delayed-inits/company.el
+++ emacs.d/delayed-inits/company.el
@@ -1,1 +1,12 @@
 (setq company-show-numbers t)
+(setq company-backends '(company-bbdb
+                         company-nxml
+                         company-css
+                         company-eclim
+                         company-semantic
+                         company-xcode
+                         company-cmake
+                         company-capf
+                         company-files
+                         (company-gtags company-keywords)
+                         company-dabbrev))
