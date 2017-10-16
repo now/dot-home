41a079c6569658d7d478bdf1e878b525db156f9a
Nikolai Weibull <now@disu.se>
2017-10-16 11:14:50 +0200

    Add *.log to revert-without-query

--- emacs.d/delayed-inits/files.el
+++ emacs.d/delayed-inits/files.el
@@ -1,3 +1,4 @@
 (setq insert-directory-program "a"
       make-backup-files nil
-      require-final-newline 'visit-save)
+      require-final-newline 'visit-save
+      revert-without-query '("\\.log$"))
