1149abbc95d573a57f42610c2c888ebf13ac894b
Nikolai Weibull <now@disu.se>
2017-12-03 11:34:12 +0100

    Use Beorg instead of MobileOrg

--- emacs.d/delayed-inits/org-mobile.el
+++ emacs.d/delayed-inits/org-mobile.el
@@ -1,3 +1,4 @@
-(setq org-mobile-directory "/scp:now@disu.se:Sites/dav/"
+(setq org-mobile-agendas nil
+      org-mobile-directory "/scp:now@disu.se:Sites/dav/"
       org-mobile-force-id-on-agenda-items nil
-      org-mobile-inbox-for-pull (concat (file-name-as-directory org-directory) "from-mobile.org"))
+      org-mobile-inbox-for-pull (concat (file-name-as-directory org-directory) "refile.org"))
