f16f4010bb251eb4c38401c8ccbff82df6852c4f
Nikolai Weibull <now@disu.se>
2018-01-22 12:56:35 +0100

    Adjust message abbrevs

--- emacs.d/delayed-inits/message.el
+++ emacs.d/delayed-inits/message.el
@@ -13,7 +13,17 @@
 ;; TODO message-subscribed-addresses?
 
 (define-abbrev message-mode-abbrev-table "br" "Best regards,\n  Nikolai"
-  :system t :case-fixed t)
+  nil :system t :case-fixed t)
+
+(define-abbrev message-mode-abbrev-table "n" "\n  Nikolai"
+  nil :system t :case-fixed t)
+
+(define-abbrev message-mode-abbrev-table "tbr"
+  "Thank you and best regards,\n  Nikolai"
+  nil :system t :case-fixed t)
+
+(define-abbrev message-mode-abbrev-table "ts" "Thanks,\n  Nikolai"
+  nil :system t :case-fixed t)
 
 (add-hook 'message-mode-hook 'now-message-mode-hook)
 
