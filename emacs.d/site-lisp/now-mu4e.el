1b4420f842a0332f384f80eaf0e2b5b4b87d1a13
Nikolai Weibull <now@disu.se>
2018-11-25 09:07:47 +0100

    Fix now-mu4e-add-mark

--- emacs.d/site-lisp/now-mu4e.el
+++ emacs.d/site-lisp/now-mu4e.el
@@ -34,8 +34,8 @@
 ;;;###autoload
 (defun now-mu4e-add-mark (mark)
   (add-to-list 'mu4e-marks mark)
-  (mu4e~headers-defun-mark-for (car mark))
-  (mu4e~view-defun-mark-for (car mark)))
+  (eval `((mu4e~headers-defun-mark-for ,(car mark))
+          (mu4e~view-defun-mark-for ,(car mark)))))
 
 ;;;###autoload
 (defun now-mu4e-headers-quit-buffer ()
