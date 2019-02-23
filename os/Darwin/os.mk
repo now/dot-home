0a4052e265842353d7789f85e71d744a6322754d
Nikolai Weibull <now@disu.se>
2019-02-23 13:06:02 +0100

    Remove mbsync launch agents

--- os/Darwin/os.mk
+++ os/Darwin/os.mk
@@ -2,10 +2,7 @@ librarydir = $(userconfdir)/Library
 librarylaunchagentsdir = $(librarydir)/LaunchAgents
 
 librarylaunchagents_DATA = \
-	   os/Darwin/Library/LaunchAgents/se.disu.kinesis.plist \
-	   os/Darwin/Library/LaunchAgents/se.disu.mbsync.daily.plist \
-	   os/Darwin/Library/LaunchAgents/se.disu.mbsync.quarterly.plist \
-	   os/Darwin/Library/LaunchAgents/se.disu.mbsync.hourly.plist
+	   os/Darwin/Library/LaunchAgents/se.disu.kinesis.plist
 
 $(call DIR,librarylaunchagents,,\
 	$$(V_LAUNCHCTL)$$(LAUNCHCTL) unload $$@; $$(LAUNCHCTL) load $$@)
