3d6ced906f8208e1860466587058a669bc0e6669
Nikolai Weibull <nikolai.weibull@pagero.com>
2021-01-05 23:34:33 +0100

    typescript-react

---
+++ emacs.d/site-lisp/typescriptreact-mode.el
@@ -0,0 +1,18 @@
+;;; typescriptreact-mode.el --- TypeScript with React language mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Nikolai Weibull
+
+;; Author: Nikolai Weibull <now@now.pagero.local>
+;; Keywords: languages
+
+;;; Code
+
+;;;###autoload
+(define-derived-mode typescriptreact-mode typescript-mode "typescriptreact"
+  "A major mode to edit TypeScript with React files.")
+
+;;;###autoload
+(add-to-list 'auto-mode-alist
+             (cons (purecopy "\\.tsx\\'") 'typescriptreact-mode))
+
+(provide typescriptreact-mode)
