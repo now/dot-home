5192f026f5556989ddb7b5225f8d9a2a7ad3b656
Nikolai Weibull <nikolai.weibull@sendify.com>
2021-11-08 10:03:41 +0100

    Install XCode command-line tools from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+out=`xcode-select --install 2>&1` || \
+  { echo "$out" | fgrep 'already installed' >/dev/null || exit 1; }
+
+# sudo xcodebuild -license
+
 install-package () {
   port -q installed "$1" | fgrep "  $1" || sudo port -N -q install "$1"
 }
