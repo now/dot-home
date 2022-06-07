7bf3895bfd3de9c481ec468f3b489054597a605d
Nikolai Weibull <now@disu.se>
2022-06-07 17:04:21 +0200

    More restructuring  of init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,20 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
-(require 'desktop)
-
 (require 'package)
 (setq package-quickstart t)
-(setf (alist-get "melpa" package-archives nil nil #'equal) "https://melpa.org/packages/")
+(setf
+ (alist-get "melpa" package-archives nil nil #'equal)
+ "https://melpa.org/packages/")
 (load (concat user-emacs-directory "packages"))
 
 (add-to-list 'load-path (concat user-emacs-directory "site-lisp"))
 (require 'userloaddefs)
 
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file)
-
-;; TODO Why?
-(setq process-connection-type nil)
+(load (setq custom-file (concat user-emacs-directory "custom.el")))
 
 ;; This comes first, as we want path set early on.
 (when (eq (window-system) 'ns)
@@ -36,127 +32,127 @@
      'light))
   (customize-set-variable 'custom-enabled-themes custom-enabled-themes))
 
-(dolist (b '("s-&"
-             ;"s-'"
-             "s-:"
-             "s-C"
-             "s-S"
-             "s-^"
-             ;"s-`"
-             "s-o"
-             "s-t"
-             "C-/"
-             "C-?"
-             "C-M-<down>"
-             "C-M-<down-mouse-1>"
-             "C-M-<drag-mouse-1>"
-             "C-M-<end>"
-             "C-M-<home>"
-             "C-M-<left>"
-             "C-M-<mouse-1>"
-             "C-M-<right>"
-             "C-M-<up>"
-             "C-<down>"
-             "C-<down-mouse-1>"
-             "C-<down-mouse-2>"
-             "C-<end>"
-             "C-<f10>"
-             "C-<home>"
-             "C-<insert>"
-             "C-<insertchar>"
-             "C-<left>"
-             "C-<next>"
-             "C-<prior>"
-             "C-<right>"
-             "C-<up>"
-             "M-<begin>"
-             "M-<down-mouse-1>"
-             "M-<drag-mouse-1>"
-             "M-<end>"
-             "M-<f10>"
-             "M-<home>"
-             "M-<left>"
-             "M-<mouse-1>"
-             "M-<mouse-2>"
-             "M-<mouse-3>"
-             "M-<next>"
-             "M-<prior>"
-             "M-<right>"
-             "M-`"
-             "S-<f10>"
-             "S-<insert>"
-             "S-<insertchar>"
-             "S-<mouse-1>"
-             "S-<mouse-3>"
-             "<XF86Back>"
-             "<XF86Forward>"
-             "<Scroll_Lock>"
-             "<again>"
-             "<begin>"
-             "<down>"
-             "<down-mouse-1>"
-             "<drag-mouse-1>"
-             "<drag-n-drop>"
-             "<end>"
-             ;"<execute>"
-             "<f1>"
-             "<f2>"
-             "<f3>"
-             "<f4>"
-             "<f10>"
-             "<f11>"
-             ;"<find>"
-             "<help>"
-             "<home>"
-             "<insert>"
-             "<insertchar>"
-             "<kp-end>"
-             "<kp-home>"
-             "<kp-next>"
-             "<kp-prior>"
-             "<left>"
-             "<menu>"
-             ;"<mouse-1>"
-             "<mouse-2>"
-             "<mouse-3>"
-             "<next>"
-             "<open>"
-             "<pinch>"
-             "<prior>"
-             "<redo>"
-             "<right>"
-             "s-<kp-bar>"
-             "s-<left>"
-             "s-<right>"
-             "<up>"
-             "ESC C-<down>"
-             "ESC C-<end>"
-             "ESC C-<home>"
-             "ESC C-<left>"
-             "ESC C-<right>"
-             "ESC C-<up>"
-             "ESC <begin>"
-             "ESC <end>"
-             "ESC <f10>"
-             "ESC <home>"
-             "ESC <left>"
-             "ESC <next>"
-             "ESC <prior>"
-             "ESC <right>"
-             "C-x C-d"
-             "C-x 6"
-             ;"C-x +"
-             ;"C-x -"
-             ;"C-x ."
-             ;"C-x ;"
-             ;"C-x `"
-             ;"C-x f"
-             "C-x C-<left>"
-             "C-x C-<right>"
-             "C-x <left>"
-             "C-x <right>"
-             ))
-  (keymap-global-unset b t))
+(dolist (key
+         '("s-&"
+           "s-:"
+           "s-C"
+           "s-S"
+           "s-^"
+                                        ;"s-`"
+           "s-o"
+           "s-t"
+           "C-/"
+           "C-?"
+           "C-M-<down>"
+           "C-M-<down-mouse-1>"
+           "C-M-<drag-mouse-1>"
+           "C-M-<end>"
+           "C-M-<home>"
+           "C-M-<left>"
+           "C-M-<mouse-1>"
+           "C-M-<right>"
+           "C-M-<up>"
+           "C-<down>"
+           "C-<down-mouse-1>"
+           "C-<down-mouse-2>"
+           "C-<end>"
+           "C-<f10>"
+           "C-<home>"
+           "C-<insert>"
+           "C-<insertchar>"
+           "C-<left>"
+           "C-<next>"
+           "C-<prior>"
+           "C-<right>"
+           "C-<up>"
+           "M-<begin>"
+           "M-<down-mouse-1>"
+           "M-<drag-mouse-1>"
+           "M-<end>"
+           "M-<f10>"
+           "M-<home>"
+           "M-<left>"
+           "M-<mouse-1>"
+           "M-<mouse-2>"
+           "M-<mouse-3>"
+           "M-<next>"
+           "M-<prior>"
+           "M-<right>"
+           "M-`"
+           "S-<f10>"
+           "S-<insert>"
+           "S-<insertchar>"
+           "S-<mouse-1>"
+           "S-<mouse-3>"
+           "<XF86Back>"
+           "<XF86Forward>"
+           "<Scroll_Lock>"
+           "<again>"
+           "<begin>"
+           "<down>"
+           "<down-mouse-1>"
+           "<drag-mouse-1>"
+           "<drag-n-drop>"
+           "<end>"
+                                        ;"<execute>"
+           "<f1>"
+           "<f2>"
+           "<f3>"
+           "<f4>"
+           "<f10>"
+           "<f11>"
+                                        ;"<find>"
+           "<help>"
+           "<home>"
+           "<insert>"
+           "<insertchar>"
+           "<kp-end>"
+           "<kp-home>"
+           "<kp-next>"
+           "<kp-prior>"
+           "<left>"
+           "<menu>"
+                                        ;"<mouse-1>"
+           "<mouse-2>"
+           "<mouse-3>"
+           "<next>"
+           "<open>"
+           "<pinch>"
+           "<prior>"
+           "<redo>"
+           "<right>"
+           "s-<kp-bar>"
+           "s-<left>"
+           "s-<right>"
+           "<up>"
+           "ESC C-<down>"
+           "ESC C-<end>"
+           "ESC C-<home>"
+           "ESC C-<left>"
+           "ESC C-<right>"
+           "ESC C-<up>"
+           "ESC <begin>"
+           "ESC <end>"
+           "ESC <f10>"
+           "ESC <home>"
+           "ESC <left>"
+           "ESC <next>"
+           "ESC <prior>"
+           "ESC <right>"
+           "C-x C-d"
+           "C-x 6"
+                                        ;"C-x +"
+                                        ;"C-x -"
+                                        ;"C-x ."
+                                        ;"C-x ;"
+                                        ;"C-x `"
+                                        ;"C-x f"
+           "C-x C-<left>"
+           "C-x C-<right>"
+           "C-x <left>"
+           "C-x <right>"
+           ))
+  (keymap-global-unset key t))
 
 (define-keymap
   :keymap (current-global-map)
@@ -175,8 +171,8 @@
   "G" 'magit-file-dispatch
   "c" 'now-project-display-compilation)
 
-(put 'narrow-to-page 'disabled nil)
-(put 'set-goal-column 'disabled nil)
+(dolist (command '(narrow-to-page set-goal-column))
+  (put command 'disabled nil))
 
 (dolist (feature-init
          '((calc . #'now-calc-init)
@@ -222,23 +218,21 @@
 
 (advice-add 'smie-auto-fill :around 'now-smie-auto-fill)
 
+(push `(,(rx ?. (or "rng" "sch" "xsd") string-end) . xml-mode) auto-mode-alist)
+
 (eval-when-compile
   (require 'find-func))
 
+(require 'desktop)
+
 (setq
- auto-mode-alist
- (cl-list*
-  '("\\.rng\\'" . xml-mode)
-  '("\\.sch\\'" . xml-mode)
-  '("\\.xsd\\'" . xml-mode)
-  auto-mode-alist)
  desktop-dirname (car desktop-path)
  find-function-C-source-directory "~/Projects/emacs/src"
  insert-directory-program "a"
  overlay-arrow-string "►"
+ process-connection-type nil            ; TODO Why?
  truncate-string-ellipsis "…")
 
-(setq-default
- semantic-function-argument-separator ", ")
+(setq-default semantic-function-argument-separator ", ")
 
 (load-theme 'now t)
